e9cba95934f76c9129b0be12920169ca680f3d71
sj2jlabicane26-sys <sj2.jlabicane26@joysistvi.edu.ph>
2026-06-20 17:49:57 +0800

    Added New Class

--- src/day3/discussion/SampleCode.java
+++ src/day3/discussion/SampleCode.java
@@ -1,13 +1,10 @@
 
-package day3.discussion;
+package day2.discussion;
 
 public class SampleCode { // class
    
     public static void main(String[] args) { // main method
         System.out.println("Hello Word"); // print statement
     }
- }
-//syntax -> structure of code
-//syntax rule
-// Main Method (psvm + tab) -> starting point of program execution
+}// Main Method (psvm + tab) -> starting point of program execution
 //print Statement(sout) -> it tells the computer to print something
