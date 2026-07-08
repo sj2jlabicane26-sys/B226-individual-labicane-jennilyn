5472149d51d332662e0161158d1b048cf73f9735
sj2jlabicane26-sys <sj2.jlabicane26@joysistvi.edu.ph>
2026-07-08 19:53:27 +0800

    Add discussion for day 18

--- src/day18/discussion/Main.java
+++ src/day18/discussion/Main.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package day18.discussion;
 
-/**
- *
- * @author Labicane
- */
+import java.util.Scanner;
 public class Main {
+     public static void main(String[] args) {
+        MethodInvocation obj = new MethodInvocation();
+        obj.b();
+        MethodInvocation.a();
+        
+        
+        Scanner scanner = new Scanner(System.in);
+        // String object
+        String word = "Hello";
+        // word.charAt(0);
+    }
     
 }
--- src/day18/discussion/Scope.java
+++ src/day18/discussion/Scope.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package day18.discussion;
 
-/**
- *
- * @author Labicane
- */
+
 public class Scope {
+    
+    int num = 10; // instance variable
+    static int num2 = 20; // static variable
     
 }
